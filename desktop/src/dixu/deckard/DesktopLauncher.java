0420c86f7129a8a20b94ae14c3909c172b85f565
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-03-15 22:57:10 +0100

    Download assets and setup DesktopLauncher

--- desktop/src/dixu/deckard/DesktopLauncher.java
+++ desktop/src/dixu/deckard/DesktopLauncher.java
@@ -8,8 +8,10 @@ import dixu.deckard.GameView;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
 		config.setForegroundFPS(60);
-		config.setTitle("Deckard-Thief");
 		new Lwjgl3Application(new GameView(), config);
 	}
 }
