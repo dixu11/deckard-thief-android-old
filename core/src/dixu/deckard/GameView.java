4dfcba55d0dc6ea76b8ddb2d05cf903496f8f819
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-03-15 23:20:25 +0100

    render desktop and background

--- core/src/dixu/deckard/GameView.java
+++ core/src/dixu/deckard/GameView.java
@@ -4,39 +4,58 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameView extends ApplicationAdapter {
-	private Texture dropImage;
-	private Texture bucketImage;
-	private Sound dropSound;
-	private Music rainMusic;
-	
-	@Override
-	public void create () {
-		dropImage = new Texture(Gdx.files.internal("droplet.png"));
-		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Rectangle bucket;
+    private Texture dropImage;
+    private Texture bucketImage;
+    private Sound dropSound;
+    private Music rainMusic;
 
-		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
-		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+    @Override
+    public void create() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+        batch = new SpriteBatch();
 
-		rainMusic.setLooping(true);
-		rainMusic.play();
-	}
+        bucket = new Rectangle(
+                800 / 2 - 64 / 2,
+                20,
+                64,
+                64
+        );
 
-	@Override
-	public void render () {
-//		ScreenUtils.clear(1, 0, 0, 1);
-//		batch.begin();
-//		batch.draw(img, 0, 0);
-//		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
+        dropImage = new Texture(Gdx.files.internal("droplet.png"));
+        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+        rainMusic.setLooping(true);
+        rainMusic.play();
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+
+        camera.update();//if we change something in camera
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(bucketImage, bucket.x, bucket.y);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
 //		batch.dispose();
 //		img.dispose();
-	}
+    }
 }
